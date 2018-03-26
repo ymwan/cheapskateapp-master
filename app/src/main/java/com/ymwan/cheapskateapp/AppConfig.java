434a0b66c260143ec14f7e08dbc8534c2a17fa6c
ymwan <35653834+ymwan@users.noreply.github.com>
2018-03-26 11:49:00 +0800

    Add files via upload

---
+++ app/src/main/java/com/ymwan/cheapskateapp/AppConfig.java
@@ -0,0 +1,10 @@
+package com.ymwan.cheapskateapp;
+
+/**
+ * Created by wanyikmun on 21/3/2018.
+ */
+
+public class AppConfig {
+
+   static String url = "http://10.212.79.235/android_login_api/";
+}
