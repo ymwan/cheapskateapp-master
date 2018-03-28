21edb226deabc80841dbb9659fc84b58d61c9766
ymwan <35653834+ymwan@users.noreply.github.com>
2018-03-28 22:17:55 +0800

    Update ShoppingList.java

--- app/src/main/java/com/ymwan/cheapskateapp/ShoppingList.java
+++ app/src/main/java/com/ymwan/cheapskateapp/ShoppingList.java
@@ -1,77 +1,36 @@
 package com.ymwan.cheapskateapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.ymwan.cheapskateapp.Adapter.DataAdapter;
-import com.ymwan.cheapskateapp.Adapter.RecyclerViewAdapter;
-
-import java.util.List;
+import android.widget.Button;
 
 /**
- * Created by wanyikmun on 21/3/2018.
+ * Created by wanyikmun on 28/3/2018.
  */
 
 public class ShoppingList extends AppCompatActivity {
 
-    private List<DataAdapter> mCartList;
-    private RecyclerViewAdapter mProductAdapter;
+    Button searchStoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.shopping_list);
 
-
-        mCartList = ShoppingListHelper.getCartList();
-
-        // Make sure to clear the selections
-        for(int i=0; i<mCartList.size(); i++) {
-            mCartList.get(i).selected = false;
-        }
-
-
-        // Create the list
-        final RecyclerView recyclerViewCatalog = (RecyclerView) findViewById(R.id.recyclerView1);
-        mProductAdapter = new RecyclerViewAdapter(mCartList, getLayoutInflater(), true);
-        recyclerViewCatalog.setAdapter(mProductAdapter);
-
-        recyclerViewCatalog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+        searchStoreButton = (Button)findViewById(R.id.btnSearchStore);
+        searchStoreButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position,
-                                    long id) {
-                Intent productDetailsIntent = new Intent(getBaseContext(),ProductDetails.class);
-                productDetailsIntent.putExtra(ShoppingListHelper.PRODUCT_INDEX, position);
-                startActivity(productDetailsIntent);
+            public void onClick(View view){
+
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=supermarket");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
-
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Refresh the data
-        if(mProductAdapter != null) {
-            mProductAdapter.notifyDataSetChanged();
-        }
-
-        double subTotal = 0;
-        for(DataAdapter p : mCartList) {
-            int quantity = ShoppingListHelper.getProductQuantity(p);
-            subTotal += p.price * quantity;
-        }
-
-        TextView productPriceTextView = (TextView) findViewById(R.id.TextViewSubtotal);
-        productPriceTextView.setText("Subtotal: $" + subTotal);
-    }
-
 }
