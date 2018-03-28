da5b5483028bcb87ec4adb0fa719922aaa04f024
ymwan <35653834+ymwan@users.noreply.github.com>
2018-03-28 22:14:39 +0800

    Update ProductDetails.java

--- app/src/main/java/com/ymwan/cheapskateapp/ProductDetails.java
+++ app/src/main/java/com/ymwan/cheapskateapp/ProductDetails.java
@@ -1,5 +1,6 @@
 package com.ymwan.cheapskateapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,9 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.analytics.ecommerce.Product;
 import com.ymwan.cheapskateapp.Adapter.DataAdapter;
 
 import java.util.List;
+
+import static com.ymwan.cheapskateapp.ViewProducts.product_date;
+import static com.ymwan.cheapskateapp.ViewProducts.product_name;
+import static com.ymwan.cheapskateapp.ViewProducts.product_price;
+import static com.ymwan.cheapskateapp.ViewProducts.product_store;
 
 /**
  * Created by wanyikmun on 25/3/2018.
@@ -24,24 +31,45 @@ public class ProductDetails extends AppCompatActivity{
 
         setContentView(R.layout.product_details);
 
-        //List<DataAdapter> catalog = ShoppingListHelper.getCatalog(getResources());
+        Intent intent = getIntent();
+        String productName = intent.getStringExtra(product_name);
+        double productPrice = intent.getDoubleExtra(product_price,0);
+        String productDate = intent.getStringExtra(product_date);
+        String productStore = intent.getStringExtra(product_store);
 
-        int productIndex = getIntent().getExtras().getInt(
+        TextView textViewName = (TextView) findViewById(R.id.TextViewProductName);
+        TextView textViewPrice = (TextView) findViewById(R.id.TextViewProductPrice);
+        TextView textViewDate = (TextView) findViewById(R.id.TextViewProductDate);
+        TextView textViewStore = (TextView) findViewById(R.id.TextViewProductStore);
+
+        textViewName.setText(productName);
+        String stringProductPrice= Double.toString(productPrice);
+        textViewPrice.setText(stringProductPrice);
+        textViewDate.setText(productDate);
+        textViewStore.setText(productStore);
+
+        //List<DataAdapter> catalog = ShoppingListHelper.getCatalog(getResources());
+       // List<Product> catalog = ShoppingListHelper.getCatalog(getResources());
+
+
+       /* int productIndex = getIntent().getExtras().getInt(
                 ShoppingListHelper.PRODUCT_INDEX);
         final DataAdapter selectedProduct = catalog.get(productIndex);
 
-        TextView productTitleTextView = (TextView) findViewById(R.id.TextViewProductTitle);
-        productTitleTextView.setText(selectedProduct.title);
+        TextView productNameTextView = (TextView) findViewById(R.id.TextViewProductName);
+        productNameTextView.setText(selectedProduct.getProductName());
         TextView productDateTextView = (TextView) findViewById(R.id.TextViewProductDate);
-        productDateTextView.setText(selectedProduct.description);
+        productDateTextView.setText(selectedProduct.getProductDate());
 
         TextView productPriceTextView = (TextView) findViewById(R.id.TextViewProductPrice);
-        productPriceTextView.setText("$" + selectedProduct.price);
+        productPriceTextView.setText("$" + selectedProduct.getProductPrice());
 
         // Update the current quantity in the cart
-       // TextView textViewCurrentQuantity = (TextView) findViewById(R.id.textViewCurrentlyInCart);
-      //  textViewCurrentQuantity.setText("Currently in Cart: "
-          //      + ShoppingCartHelper.getProductQuantity(selectedProduct));
+       TextView textViewCurrentQuantity = (TextView) findViewById(R.id.textViewCurrentlyInCart);
+        textViewCurrentQuantity.setText("Currently in Cart: "
+              + ShoppingListHelper.getProductQuantity(selectedProduct));
+
+      */
 
         // Save a reference to the quantity edit text
         final EditText editTextQuantity = (EditText) findViewById(R.id.editTextQuantity);
@@ -74,7 +102,7 @@ public class ProductDetails extends AppCompatActivity{
                 }
 
                 // If we make it here, a valid quantity was entered
-                ShoppingListHelper.setQuantity(selectedProduct, quantity);
+                //ShoppingListHelper.setQuantity(selectedProduct, quantity);
 
                 // Close the activity
                 finish();
@@ -84,4 +112,4 @@ public class ProductDetails extends AppCompatActivity{
     }
 
 }
-}
+
